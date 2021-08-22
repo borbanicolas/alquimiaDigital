33f0e58ff5a09f15a0a523c766dab0a4c0feff95
borbanicolas <86000717+borbanicolas@users.noreply.github.com>
2021-08-22 11:24:14 -0300

    comecei o bloco de clientes

--- js/mais.js
+++ js/mais.js
@@ -1,5 +1,4 @@
-
-window.onscroll = function() {
+    window.onscroll = function() {
     let header = document.getElementById('header');
     if ( window.pageYOffset > 85 ) {
         header.classList.add("active");
