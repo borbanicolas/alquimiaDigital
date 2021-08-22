2e7c1e7c90576b8cbc340173b76263a02e4a269e
borbanicolas <86000717+borbanicolas@users.noreply.github.com>
2021-08-22 10:05:31 -0300

    teste

--- js/mais.js
+++ js/mais.js
@@ -1,20 +1,10 @@
-/* jQuery(document).ready(function($) {
-    window.onscroll = function(){
-        if (window.pageYOffset > 140){
-            $('#header').addClass("active");
-        }else{
-            $('#header').removeClass("active");
-        }
-    }
-} */
-console.log()
-
 
 window.onscroll = function() {
     let header = document.getElementById('header');
-    if ( window.pageYOffset > 140 ) {
+    if ( window.pageYOffset > 85 ) {
         header.classList.add("active");
     } else {
         header.classList.remove("active");
     }
 }
+
