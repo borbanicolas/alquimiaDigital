0e43b6562c85877375f70befbdd82a146a4fc4ec
borbanicolas <86000717+borbanicolas@users.noreply.github.com>
2021-08-24 14:04:29 -0300

    trabalhando no responsivo do bloco de clientes

--- js/mais.js
+++ js/mais.js
@@ -1,6 +1,6 @@
     window.onscroll = function() {
     let header = document.getElementById('header');
-    if ( window.pageYOffset > 85 ) {
+    if ( window.pageYOffset > 10 ) {
         header.classList.add("active");
     } else {
         header.classList.remove("active");
