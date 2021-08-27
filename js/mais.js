df4aeebd4e7d0935069038e0d97419280a96672c
borbanicolas <86000717+borbanicolas@users.noreply.github.com>
2021-08-26 22:09:08 -0300

    criando filtro com js

--- js/mais.js
+++ js/mais.js
@@ -7,12 +7,49 @@
     }
 }
 
-let btnsContainer = document.querySelector('#cases .button-group')
-let btn = document.querySelectorAll('button')
+/*let btnsContainer = document.querySelector('#cases .button-group ')
+let btn = document.querySelectorAll('button ')
+
 
 btn.forEach(el => {
     el.addEventListener('click', function(){
         btnsContainer.querySelector('.active').classList.remove('active');
         el.classList.add('active');
+
+        let filtro = el.textContent;
+        btnContainer.forEach(btn => {
+            btnContainer.style.display = none;
+            if (btnContainer.getAttribute('data-filter') == filtro.toLowerCase() || filtro == "All Menu"){
+                btnContainer.style.display = 'block';
+            }
+        })
+
+         
+      
     });
-});
+}); */
+
+        let btnscontainer = document.querySelectorAll('#cases .button-group button')
+        let imgItem = document.querySelectorAll('.row  .element-item img ')
+
+        btnscontainer.forEach(button => {
+            button.onclick = function(){
+                btnscontainer.forEach(button => {
+                    button.className = ""
+
+                })
+                button.className = "active" 
+                
+                let filtroDoAgoraVai = button.textContent
+                imgItem.forEach(img => {
+                    img.style.display = 'none'
+                    if(img.getAttribute('data-filter') == filtroDoAgoraVai.toLowerCase() || filtroDoAgoraVai == "Todos"){
+                        img.style.display = 'block'
+                    }
+                })
+            }
+        })
+
+
+
+
